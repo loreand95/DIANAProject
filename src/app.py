24b67e6e6240586cbde6da84cf7c0dc2bd820108
loreand <andreoli95@gmail.com>
2021-09-03 10:47:55 +0200

    add colors

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, redirect, url_for
 from routes.search_bp import search_bp
-from flask import render_template
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
